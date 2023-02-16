92431e684a8f1781e31dbe3cf7401ef729ba7a6f
unconv <unconventionalcoding@gmail.com>
2023-02-16 03:19:15 +0200

    updated prompts

--- src/CategorizerChatWTF.php
+++ src/CategorizerChatWTF.php
@@ -8,7 +8,7 @@ class CategorizerChatWTF extends ChatWTF
     public function __construct(
         protected OpenAi $open_ai_api,
     ) {
-        $this->set_default_prompt( "Create a list of 1-5 categories for the given thing, for example: celebrity, houshold item, singer, actor, actress, musician, tool, animal, pet. Give the category as a singular noun. Don't add numberings. One category per line.\n\nThing: zebra\nThe categories for 'zebra' are:\n- Animal\n- Exotic animal\n\nThing: computer keyboard\nThe categories for 'computer keyboard' are:\n- Computer accessory\n- Technology\n\nThing: airplane\nThe categories for 'airplane' are:\n- Aircraft\n- Technology\n- Large machines\n\nThing: {object}\nThe categories for '{object}' are:\n" );
+        $this->set_default_prompt( "Create a list of 10 categories for the given thing, for example: celebrity, household item, singer, actor, actress, musician, tool, animal, pet or politician. Give the category as a singular noun. Don't add numberings. One category per line.\nThe categories for '{object}' are:\n" );
     }
 
     protected function create_prompt( string $question ) {
--- src/TopCategorizerChatWTF.php
+++ src/TopCategorizerChatWTF.php
@@ -8,7 +8,7 @@ class TopCategorizerChatWTF extends ChatWTF
     public function __construct(
         protected OpenAi $open_ai_api,
     ) {
-        $this->set_default_prompt( "Select a top level category for the given category out of these choices: person, object, animal, abstract idea, technology. The top level category for '{object}' is:\n" );
+        $this->set_default_prompt( "Select a top level category for the given category out of these choices: person, object, animal, software, other.\n\nThe top level category for 'Activist' is: Person\nThe top level category for 'Philanthropist' is: Person\nThe top level category for 'Color' is: Other\nThe top level category for 'Amphibian' is: Animal\nThe top level category for '{object}' is: " );
     }
 
     protected function create_prompt( string $question ) {
