2fc231a3f6fd0bcc537abcb4b496482ca86ec780
unconv <unconventionalcoding@gmail.com>
2023-02-16 14:40:45 +0200

    changed categorization prompt

--- src/CategorizerChatWTF.php
+++ src/CategorizerChatWTF.php
@@ -8,7 +8,7 @@ class CategorizerChatWTF extends ChatWTF
     public function __construct(
         protected OpenAi $open_ai_api,
     ) {
-        $this->set_default_prompt( "Create a semicolon separated list of categories for the given thing, for example: celebrity, household item, singer, actor, actress, musician, tool, animal, person, pet or politician. Give the category as a singular noun. Use simple words that anyone would know. The category must have an 'is a' relationship with the object so it must fit the sentence '<Object> is a <Category>'. Don't add numberings. \nThe categories for '{object}' are:\n" );
+        $this->set_default_prompt( "Create a semicolon separated list of categories for the given thing, for example: celebrity, household item, singer, actor, actress, musician, tool, animal, person, pet or politician. Give the category as a singular noun. Use simple words that anyone would know. For an animal, add its dietary classification as a category. The category must have an 'is a' relationship with the object so it must fit the sentence '<Object> is a <Category>'. Don't add numberings. \nThe categories for '{object}' are:\n" );
     }
 
     protected function create_prompt( string $question ) {
